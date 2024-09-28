3c038e9fb259c243a82baffd409686fe701af5c6
Lucas Gabriel <lucasgabrielgomes33@gmail.com>
2024-09-28 18:05:15 -0300

    Craete Home and Topo screen

---
+++ src/views/Home/Home.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { View, StyleSheet, Text, Image } from "react-native";
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import Topo from "./components/Topo";
+
+import cachorro from "../../../assets/img/cachorro01.png";
+
+export default function Home() {
+     return <SafeAreaView style={estilos.safeArea}>
+          <View style={estilos.container}>
+               <Topo/>
+               <View style={estilos.card}>
+                    <Text style={estilos.textoCard}>Ver instruções de uso</Text>
+                    <Image style={estilos.imagemCard} source={cachorro} accessibilityLabel="Cachorro"/>
+               </View>
+          </View>
+     </SafeAreaView>
+     
+}
+
+const estilos = StyleSheet.create({
+     safeArea: {
+          flex: 1,
+          backgroundColor: "#F1F1F1",
+     },
+     container: {
+          paddingHorizontal: 24,
+          backgroundColor: "#F1F1F1",
+          rowGap: 16
+     },
+     card: {
+          height: 100,
+          width: "100%",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+          padding: 16,
+          backgroundColor: "#ffffff",
+          borderRadius: 6,
+          elevation: 4
+     },
+     textoCard: {
+          fontSize: 16,
+          lineHeight: 26,
+          fontWeight: "bold"
+     },
+     imagemCard: {
+          height: 60,
+          width: 60
+     }
+});
---
+++ src/views/Home/components/Topo.js
@@ -0,0 +1,30 @@
+import React from "react-native";
+import { View, Image, StyleSheet } from "react-native";
+
+import logo from "../../../../assets/img/logo-horizontal.png";
+import profileDefault from "../../../../assets/img/profile-default.png";
+
+export default function Topo() {
+     return <View style={estilos.topo}>
+          <Image source={logo} style={estilos.imgLogo} accessibilityLabel="DogPic"/>
+          <Image source={profileDefault} style={estilos.imgProfile} accessibilityLabel="Perfil"/>
+     </View>
+}
+
+const estilos = StyleSheet.create({
+     topo: {
+          width: "100%",
+          height: 80,
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+     },
+     imgLogo: {
+          width: 105,
+          height: 40
+     },
+     imgProfile: {
+          height: 38,
+          width: 38
+     }
+});
