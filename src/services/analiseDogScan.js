f8632319184cc5c4fce87feda7f6d44d204780d6
Lucas Gabriel <lucasgabrielgomes33@gmail.com>
2024-11-21 20:23:34 -0300

    Modification to files

--- src/services/analiseDogScan.js
+++ src/services/analiseDogScan.js
@@ -1,11 +1,4 @@
-import axios from "axios";
-
-// Credenciais do modelo do Clarifai
-const PAT = '88174510e2b74d99bac75a454c6c4c52';
-const USER_ID = 'datastrategy';       
-const APP_ID = 'dog-breeds';
-const MODEL_ID = 'dog-breeds-classifier';
-const MODEL_VERSION_ID = '2264a9846bae46f3956f1b8733469883';
+import { PAT, USER_ID, APP_ID, MODEL_ID, MODEL_VERSION_ID } from "../config/clarifai"; 
 
 export const analiseDogClarifai = async (base64Image) => {
      const raw = JSON.stringify({
